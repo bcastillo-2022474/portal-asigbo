336fec7a08afb3cb8c5600697154c1f95cd19ae9
Diego <diegoguatedb2002@gmail.com>
2023-06-09 12:42:40 -0600

    configuración de proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [eslint(), react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+      { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+      { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+      { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
+      { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+      { find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
+      { find: '@helpers', replacement: path.resolve(__dirname, './src/helpers') },
+
+    ],
+  },
+});
