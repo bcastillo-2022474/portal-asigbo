d1166ab3fa982a50f755b96aafc9e2c53ab4f7e1
hsilv <hm.sebastiansilva@gmail.com>
2023-06-14 18:29:22 -0600

    Creación de favicon

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+/* import { BrowserRouter as Router } from 'react-router-dom';
 import IndexPage from '../../pages/IndexPage/IndexPage';
-import { SessionProvider } from '../../context/SessionContext';
+import { SessionProvider } from '../../context/SessionContext'; */
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   return (
-    <SessionProvider>
-      <Router>
-        <IndexPage />
-      </Router>
-    </SessionProvider>
+    <NavBar />
   );
 }
 
 export default App;
 /* <div>App</div> */
+/* <SessionProvider>
+      <Router>
+        <IndexPage />
+      </Router>
+    </SessionProvider> */
