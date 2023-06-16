d3a9a11cbc9c46be46a3b61f26e764a8368475e5
pabloozamora <pabloozamora02@gmail.com>
2023-06-16 02:24:39 -0600

    Inicio página crear usuarios (admin)

--- src/pages/AdminIndexPage/AdminIndexPage.jsx
+++ src/pages/AdminIndexPage/AdminIndexPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AdminProfilePage from '../AdminProfilePage/AdminProfilePage';
+import NewUserPage from '../NewUserPage/NewUserPage';
 
 function AdminIndexPage() {
   return (
     <Routes>
       <Route path="/" element={<AdminProfilePage />} />
+      <Route path="/newUser" element={<NewUserPage />} />
     </Routes>
   );
 }
---
+++ src/pages/NewUserPage/NewUserPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './NewUserPage.module.css';
+
+function NewUserPage() {
+  return (
+    <div className={styles.newUserPage}>Nuevo usuario</div>
+  );
+}
+
+export default NewUserPage;
