996b7f22245e1ae7e7eccfcb17ad7770b03043bf
pabloozamora <pabloozamora02@gmail.com>
2023-06-21 04:22:31 -0600

    Más correcciones login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,6 +3,7 @@ import InputText from '@components/InputText/InputText';
 import Spinner from '@components/Spinner/Spinner';
 import waves from '@assets/wave-haikei.svg';
 import logo from '@assets/asigboazul.png';
+import logoMobile from '@assets/General/Copia de Transparente (letras blancas).png';
 import { button, blue } from '@styles/buttons.module.css';
 import useLogin from '@hooks/useLogin';
 import styles from './LoginPage.module.css';
@@ -52,9 +53,13 @@ function LoginPage() {
   return (
     <div className={styles.loginPageContainer}>
       <div className={styles.formContainer}>
-        <div className={styles.logoContainer} />
-        <div className={styles.wavesWrapperMobile}>
-          <svg className={styles.wavesMobile} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#26397B" fillOpacity="1" d="M0,128L48,117.3C96,107,192,85,288,85.3C384,85,480,107,576,128C672,149,768,171,864,160C960,149,1056,107,1152,101.3C1248,96,1344,128,1392,144L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z" /></svg>
+        <div className={styles.headerContainer}>
+          <div className={styles.logoContainer}>
+            <img className={styles.logoAsigboMobile} alt="logo" src={logoMobile} />
+          </div>
+          <div className={styles.wavesWrapperMobile}>
+            <svg className={styles.wavesMobile} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#16337F" fillOpacity="1" d="M0,128L48,117.3C96,107,192,85,288,85.3C384,85,480,107,576,128C672,149,768,171,864,160C960,149,1056,107,1152,101.3C1248,96,1344,128,1392,144L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z" /></svg>
+          </div>
         </div>
         <div className={styles.form}>
           <img alt="ASIGBO logo" className={styles.logoAsigbo} src={logo} />
@@ -92,7 +97,7 @@ function LoginPage() {
         </div>
       </div>
       <div className={styles.wavesWrapper}>
-        <img alt="waves" className={waves} src={waves} />
+        <img alt="waves" className={styles.waves} src={waves} />
       </div>
       <div className={styles.backgroundWrapper}>
         <div className={styles.backgroundFilter} />
