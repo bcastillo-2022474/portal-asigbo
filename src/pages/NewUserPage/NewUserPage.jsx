3205b02a14e921c531c46036b74d656ba16ed2f2
pabloozamora <pabloozamora02@gmail.com>
2023-09-11 03:22:44 -0600

    Creación de usuarios

--- src/pages/NewUserPage/NewUserPage.jsx
+++ src/pages/NewUserPage/NewUserPage.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import NavMenuButton from '@components/PageContainer/NavMenuButton/NavMenuButton';
 import InputText from '@components/InputText';
 import ExcelIcon from '@assets/icons/ExcelIcon';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../../components/Spinner/Spinner';
 import Button from '../../components/Button/Button';
-import InputSearchSelect from '../../components/InputSearchSelect/InputSearchSelect';
-import LoadingView from '../../components/LoadingView/LoadingView';
+import InputSelect from '../../components/InputSelect/InputSelect';
+import InputNumber from '../../components/InputNumber/InputNumber';
 import styles from './NewUserPage.module.css';
 import useForm from '../../hooks/useForm';
 import useFetch from '../../hooks/useFetch';
@@ -25,15 +25,6 @@ function NewUserPage() {
     form, error, setData, validateField, clearFieldError, validateForm,
   } = useForm(createUserSchema);
 
-  const [promotions, setPromotions] = useState([]);
-
-  const {
-    callFetch: fetchPromotions,
-    result: resultPromotions,
-    error: errorPromotions,
-    loading: loadingPromotions,
-  } = useFetch();
-
   const {
     callFetch: postUser,
     result: resultPostUser,
@@ -44,17 +35,12 @@ function NewUserPage() {
   const [isSuccessOpen, openSuccess, closeSuccess] = usePopUp();
   const [isErrorOpen, openError, closeError] = usePopUp();
 
-  const getPromotions = () => {
-    fetchPromotions({
-      uri: `${serverHost}/promotion/`,
-      headers: { authorization: token },
-    });
-  };
-
   const redirectOnSuccess = () => navigate('/');
 
   const handleSubmitUser = async (e) => {
     e.preventDefault();
+
+    setData('code', Math.floor(Math.random() * (15001 - 0) + 0));
 
     const formErrors = await validateForm();
     if (formErrors) return;
@@ -63,7 +49,7 @@ function NewUserPage() {
       uri: `${serverHost}/user/`,
       method: 'POST',
       headers: { authorization: token },
-      body: form,
+      body: JSON.stringify(form),
     });
   };
 
@@ -73,26 +59,9 @@ function NewUserPage() {
 
   const handleFormChange = (e) => {
     const { name, value } = e.target;
-    console.log(name, value);
+    // console.log(name, value);
     setData(name, value);
   };
-
-  useEffect(() => {
-    if (!resultPromotions) return;
-    const promotionsArray = [];
-    resultPromotions.students.years.forEach((promotion) => {
-      promotionsArray.push({ value: `${promotion}`, title: `${promotion}` });
-    });
-    setPromotions(promotionsArray);
-  }, [resultPromotions]);
-
-  useEffect(() => {
-    console.log(errorPromotions);
-  }, [errorPromotions]);
-
-  useEffect(() => {
-    getPromotions();
-  }, []);
 
   useEffect(() => {
     if (errorPostUser) openError();
@@ -104,7 +73,6 @@ function NewUserPage() {
 
   return (
     <div className={styles.newUserPage}>
-      {loadingPromotions && <LoadingView />}
       <div className={styles.headerContainer}>
         <h1 className={styles.pageTitle}>Crear usuario</h1>
         <div className={styles.iconWrapper}>
@@ -126,12 +94,12 @@ function NewUserPage() {
         />
         <InputText
           title="Apellidos del becado"
-          name="lastName"
-          value={form?.lastName}
-          error={error?.lastName}
+          name="lastname"
+          value={form?.lastname}
+          error={error?.lastname}
           onChange={handleFormChange}
-          onFocus={() => clearFieldError('lastName')}
-          onBlur={() => validateField('lastName')}
+          onFocus={() => clearFieldError('lastname')}
+          onBlur={() => validateField('lastname')}
         />
         <InputText
           title="Correo electrónico del becado"
@@ -151,25 +119,27 @@ function NewUserPage() {
           onFocus={() => clearFieldError('career')}
           onBlur={() => validateField('career')}
         />
-        <InputSearchSelect
-          options={promotions}
+        <InputNumber
           name="promotion"
           value={form?.promotion}
           error={error?.promotion}
           onChange={handleFormChange}
           onFocus={() => clearFieldError('promotion')}
           onBlur={() => validateField('promotion')}
-          placeholder="Promoción"
+          title="Promoción"
+          min={2000}
+          max={2100}
         />
-        <InputSearchSelect
+        <InputSelect
           options={[{ value: 'M', title: 'Masculino' }, { value: 'F', title: 'Femenino' }]}
           name="sex"
-          value={form?.sex}
           error={error?.sex}
           onChange={handleFormChange}
           onFocus={() => clearFieldError('sex')}
           onBlur={() => validateField('sex')}
           placeholder="Sexo"
+          title="Sexo"
+          value={form?.sex}
         />
         <div className={styles.sendContainer}>
           {!resultPostUser && !loadingPostUser && (
@@ -183,7 +153,7 @@ function NewUserPage() {
         close={closeSuccess}
         isOpen={isSuccessOpen}
         callback={redirectOnSuccess}
-        text="El nuevo eje de Asigbo ha sido creado de forma exitosa."
+        text="El usuario del becado ha sido creado de forma exitosa."
       />
 
       <ErrorNotificationPopUp
