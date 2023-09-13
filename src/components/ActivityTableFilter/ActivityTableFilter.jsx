24315ab0129aba6734d92d58f81a074ea983a1b4
hsilv <hm.sebastiansilva@gmail.com>
2023-09-12 21:22:15 -0600

    documentación2xd

--- src/components/ActivityTableFilter/ActivityTableFilter.jsx
+++ src/components/ActivityTableFilter/ActivityTableFilter.jsx
@@ -4,6 +4,24 @@ import styles from './ActivityTableFilter.module.css';
 import InputSearchDate from '../InputSearchDate';
 import SearchInput from '../SearchInput/SearchInput';
 
+/*----------------------------------------------------------------------------------------------*/
+
+/**
+ * @module ActivityTableFilter: Componente destinado a manejar los **valores** sobre los cuales
+ * se filtrará un objeto de actividades, incluye rango de fechas y búsqueda por texto.
+ *
+ * @param {function} searchHandler: Función a la que se le pasará el parámetro de búsqueda como
+ * parámetro de la misma.
+ * @param {function} initialDateHandler: Función a la que se le pasará la cota inferior de fecha,
+ * es decir **DESDE** qué fecha se filtrará.
+ * @param {function} finalDateHandler: Función a la que se la pasará la cota superior de fecha,
+ * es decir **HASTA** qué fecha se filtrará.
+ *
+ * @requires <SearchInput/>,<InputSearchDate/>
+ */
+
+/*----------------------------------------------------------------------------------------------*/
+
 function ActivityTableFilter({ searchHandler, initialDateHandler, finalDateHandler }) {
   const [initialDate, setInitialDate] = useState();
   const [finalDate, setFinalDate] = useState();
@@ -33,6 +51,8 @@ function ActivityTableFilter({ searchHandler, initialDateHandler, finalDateHandl
   );
 }
 
+/*----------------------------------------------------------------------------------------------*/
+
 ActivityTableFilter.propTypes = {
   searchHandler: PropTypes.func,
   initialDateHandler: PropTypes.func,
@@ -44,5 +64,7 @@ ActivityTableFilter.defaultProps = {
   initialDateHandler: null,
   finalDateHandler: null,
 };
+
+/*----------------------------------------------------------------------------------------------*/
 
 export default ActivityTableFilter;
