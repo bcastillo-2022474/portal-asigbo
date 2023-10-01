ef9268adb458ef94e97432b0ec4f0ece56157bcf
Diego <diegoguatedb2002@gmail.com>
2023-09-30 20:58:12 -0600

    fix: not found y home page en index de user

--- src/pages/UserIndexPage/UserIndexPage.jsx
+++ src/pages/UserIndexPage/UserIndexPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PageContainer from '../../components/PageContainer/PageContainer';
 import HomePage from '../HomePage/HomePage';
+import NotFoundPage from '../NotFoundPage';
 
 function UserIndexPage() {
   return (
     <PageContainer>
       <Routes>
-        <Route path="/:userId" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </PageContainer>
   );
