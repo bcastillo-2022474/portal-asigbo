437a6cc53d5d06c3b3070a5b317e79ff76b40913
Diego <diegoguatedb2002@gmail.com>
2023-09-30 20:27:38 -0600

    fix: home page agregada en index de usuario

--- src/pages/UserIndexPage/UserIndexPage.jsx
+++ src/pages/UserIndexPage/UserIndexPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import UserProfilePage from '../UserProfilePage/UserProfilePage';
 import PageContainer from '../../components/PageContainer/PageContainer';
+import HomePage from '../HomePage/HomePage';
 
 function UserIndexPage() {
   return (
     <PageContainer>
       <Routes>
-        <Route path="/:userId" element={<UserProfilePage />} />
+        <Route path="/:userId" element={<HomePage />} />
       </Routes>
     </PageContainer>
   );
