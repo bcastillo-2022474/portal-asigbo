470285dfab1e6d8d7bfd64b730818546b22407fa
Diego <diegoguatedb2002@gmail.com>
2023-10-01 20:37:22 -0600

    fix: ocultar botón de crear eje según rol

--- src/pages/AreasListPage/AreasListPage.jsx
+++ src/pages/AreasListPage/AreasListPage.jsx
@@ -4,15 +4,21 @@ import { NavLink } from 'react-router-dom';
 import styles from './AreasListPage.module.css';
 import AreasList from '../../components/AreasList/AreasList';
 import Button from '../../components/Button/Button';
+import useSessionData from '../../hooks/useSessionData';
+import consts from '../../helpers/consts';
 
 function AreasListPage() {
+  const sessionUser = useSessionData();
+
   return (
     <div className={styles.globalConfigPage}>
       <div className={styles.pageHeader}>
         <h1>Ejes de ASIGBO</h1>
+        {sessionUser?.role.includes(consts.roles.admin) && (
         <NavLink to="/area/nuevo" className={styles.newLink}>
           <Button text="Nuevo" />
         </NavLink>
+        )}
       </div>
       <AreasList />
     </div>
