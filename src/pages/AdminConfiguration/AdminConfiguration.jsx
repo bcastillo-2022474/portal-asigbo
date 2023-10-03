fa75b94c36cd603a67f50f0ba3019e46561f93e0
Diego <diegoguatedb2002@gmail.com>
2023-10-03 14:00:15 -0600

    feat: lista para asignar encargados de promoción

--- src/pages/AdminConfiguration/AdminConfiguration.jsx
+++ src/pages/AdminConfiguration/AdminConfiguration.jsx
@@ -4,12 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import TabMenu from '@components/TabMenu/TabMenu';
 import SelectAdminTable from '@components/SelectAdminTable';
 import styles from './AdminConfiguration.module.css';
+import SelectPromotionResponsibleTable from '../../components/SelectPromotionResponsibleTable';
 
 function AdminConfiguration() {
   return (
     <div className={styles.adminConfiguration}>
       <h1 className={styles.pageTitle}>Configuración</h1>
-      <TabMenu className={styles.tabMenu} options={[{ text: 'General', href: '' }, { text: 'Administradores', href: 'admin' }]} />
+      <TabMenu className={styles.tabMenu} options={[{ text: 'General', href: '' }, { text: 'Administradores', href: 'admin' }, { text: 'Encargados de año', href: 'encargados_promocion' }]} />
       <Routes>
 
         <Route
@@ -19,6 +20,17 @@ function AdminConfiguration() {
             <>
               <h3 className={styles.sectionTitle}>Administradores</h3>
               <SelectAdminTable />
+            </>
+)}
+        />
+
+        <Route
+          end
+          path="/encargados_promocion"
+          element={(
+            <>
+              <h3 className={styles.sectionTitle}>Encargados de año</h3>
+              <SelectPromotionResponsibleTable />
             </>
 )}
         />
