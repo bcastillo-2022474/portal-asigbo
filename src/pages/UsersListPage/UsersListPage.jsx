3ea76d30c68b0d03475e6eb500e3a118dae4f44b
pabloozamora <pabloozamora02@gmail.com>
2023-10-19 04:38:47 -0600

    feat: newUser button

--- src/pages/UsersListPage/UsersListPage.jsx
+++ src/pages/UsersListPage/UsersListPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './UsersListPage.module.css';
 import ManageUsersTable from '../../components/ManageUsersTable/ManageUsersTable';
+import Button from '../../components/Button/Button';
 
 function UsersListPage() {
+  const navigate = useNavigate();
+
+  // Redirigir a nuevo usuario
+  const handleNewUser = () => {
+    navigate('/usuario/nuevo');
+  };
+
   return (
     <div className={styles.usersListPage}>
-      <h1 className={styles.pageTitle}>Lista de usuarios</h1>
+      <div className={styles.header}>
+        <h1 className={styles.pageTitle}>Lista de usuarios</h1>
+        <Button text="Nuevo usuario" onClick={handleNewUser} className={styles.newUserButton} />
+      </div>
       <ManageUsersTable />
     </div>
   );
