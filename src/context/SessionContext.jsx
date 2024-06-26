23d386522581fed2d212ec6eb330702e7c64f052
Aq202 <diegoguatedb2002@gmail.com>
2024-06-26 15:35:21 -0600

    fix: bug en auto refresh token corregido

--- src/context/SessionContext.jsx
+++ src/context/SessionContext.jsx
@@ -1,4 +1,6 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, {
+  createContext, useEffect, useRef, useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import { serverHost } from '../config';
 
@@ -16,6 +18,8 @@ import { serverHost } from '../config';
  * -- -- obtenido y null será que la sesión no existe o no es válida.
  * -- -- @property {React.SetStateAction} setAccessToken: Hook/Función para modificar el estado
  * -- -- actual del accessToken.
+ * -- -- @property {React.ref} accessTokenRef: {token, refreshing} Hook ref para almacenar el token
+ * -- -- refrescado y estado de refrescamiento.
  *
  * @exports
  * -- NamedExport -> @property {JSX.Element} SessionProvider: Proveedor de sesión
@@ -29,23 +33,46 @@ function SessionProvider({ children }) {
   // null es que la sesión no existe
   const [accessToken, setAccessToken] = useState(undefined);
 
+  // Hook ref para almacenar el token refrescado y estado de refrescamiento
+  const accessTokenRef = useRef({
+    token: null,
+    refreshing: false,
+  });
+
   // eslint-disable-next-line react/jsx-no-constructed-context-values
   const data = {
     accessToken,
     setAccessToken,
+    accessTokenRef,
   };
 
   useEffect(() => {
     // obtener access token inicial
     const uri = `${serverHost}/session/accessToken`;
+
+    accessTokenRef.current = {
+      token: null,
+      refreshing: true,
+    };
+
     (async () => {
       try {
         const res = await fetch(uri);
         if (!res.ok) throw res;
         const { accessToken: receivedAccessToken } = await res.json();
         setAccessToken(receivedAccessToken);
+
+        accessTokenRef.current = {
+          token: receivedAccessToken,
+          refreshing: false,
+        };
       } catch (ex) {
         setAccessToken(null);
+
+        accessTokenRef.current = {
+          token: null,
+          refreshing: false,
+        };
       }
     })();
   }, []);
